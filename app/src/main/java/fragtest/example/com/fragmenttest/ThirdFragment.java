0f34cb151b2351c4439a7cc76c3eb29a9d8f865a
root0301 <2648398526@qq.com>
2016-08-02 20:13:06 +0800

    FragmentTest

---
+++ app/src/main/java/fragtest/example/com/fragmenttest/ThirdFragment.java
@@ -0,0 +1,21 @@
+package fragtest.example.com.fragmenttest;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by slience on 2016/8/1.
+ */
+public class FirstFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1,container,false);
+    }
+
+}
